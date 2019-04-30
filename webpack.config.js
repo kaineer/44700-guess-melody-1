1aeb802e18f62aea3743052a0c7cc0d1e6b8943d
Sergey Kluchkovsky <kaineer@gmail.com>
2019-04-30 14:05:30 +0500

    Добавляет настройку расширений

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,16 @@ module.exports = {
   module: {
     rules: [
       {
-	test: /\.jsx?$/,
-	exclude: /node_modules/,
-	use: {
-	  loader: `babel-loader`
-	}
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`
+        }
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   devtool: `source-map`
 };
