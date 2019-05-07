0570a9634a6efc4435809466917ed6af4a0c82cd
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-07 15:16:09 +0500

    Исправляет имена используемых атрибутов

--- src/components/guess-artist-item/guess-artist-item.jsx
+++ src/components/guess-artist-item/guess-artist-item.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {string, number, func} from 'prop-types';
 
-export const GuessArtistItem = ({artist, src, orderId, onClick}) => (
+export const GuessArtistItem = ({artist, picture, orderId, onClick}) => (
   <div className="artist">
     <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${orderId}`} id={`answer-${orderId}`}
       onClick={onClick}
     />
     <label className="artist__name" htmlFor={`answer-${orderId}`}>
-      <img className="artist__picture" src={src} alt={artist} />
+      <img className="artist__picture" src={picture} alt={artist} />
       {artist}
     </label>
   </div>
@@ -15,7 +15,7 @@ export const GuessArtistItem = ({artist, src, orderId, onClick}) => (
 
 GuessArtistItem.propTypes = {
   artist: string.isRequired,
-  src: string.isRequired,
+  picture: string.isRequired,
   orderId: number.isRequired,
   onClick: func
 };
