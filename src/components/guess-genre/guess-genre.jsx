0b5ed76d9fd350bf8badca6d0968f78aaeced6cf
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-07 12:51:51 +0500

    Добавляет экран «отгадай жанр»

---
+++ src/components/guess-genre/guess-genre.jsx
@@ -0,0 +1,42 @@
+// src/components/guess-genre/guess-genre.jsx
+
+import React, {Component} from 'react';
+import {string, shape, arrayOf} from 'prop-types';
+import {GameHeader} from '../game-header/game-header';
+import {GuessGenreItem} from '../guess-genre-item/guess-genre-item';
+
+export class GuessGenre extends Component {
+  render() {
+    const {answers} = this.props;
+
+    return (
+      <section className="game game--genre">
+        <GameHeader />
+
+        <section className="game__screen">
+          <h2 className="game__title">Выберите инди-рок треки</h2>
+          <form className="game__tracks">
+            {answers.map(({src, genre}, orderId) => (
+              <GuessGenreItem
+                key={orderId}
+                src={src}
+                genre={genre}
+                orderId={orderId}
+              />
+            ))}
+
+            <button className="game__submit button" type="submit">Ответить</button>
+          </form>
+        </section>
+      </section>
+    );
+  }
+}
+
+GuessGenre.propTypes = {
+  genre: string,
+  answers: arrayOf(shape({
+    src: string,
+    genre: string
+  }))
+};
