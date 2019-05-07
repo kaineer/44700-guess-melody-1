f6d8896ec30d34e8f4bf731ec7b46dafc0ae2197
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-07 12:50:58 +0500

    Выносит заголовок из GuessArtist

--- src/components/guess-artist/guess-artist.jsx
+++ src/components/guess-artist/guess-artist.jsx
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 import {string, shape, arrayOf} from 'prop-types';
+import {GameHeader} from '../game-header/game-header';
 import {GuessArtistItem} from '../guess-artist-item/guess-artist-item';
 
 export class GuessArtist extends Component {
@@ -10,28 +11,7 @@ export class GuessArtist extends Component {
 
     return (
       <section className="game game--artist">
-        <header className="game__header">
-          <a className="game__back" href="#">
-            <span className="visually-hidden">Сыграть ещё раз</span>
-            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-          </a>
-
-          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle className="timer__line" cx="390" cy="390" r="370" style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center" />
-          </svg>
-
-          <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-            <span className="timer__mins">05</span>
-            <span className="timer__dots">:</span>
-            <span className="timer__secs">00</span>
-          </div>
-
-          <div className="game__mistakes">
-            <div className="wrong"></div>
-            <div className="wrong"></div>
-            <div className="wrong"></div>
-          </div>
-        </header>
+        <GameHeader />
 
         <section className="game__screen">
           <h2 className="game__title">Кто исполняет эту песню?</h2>
