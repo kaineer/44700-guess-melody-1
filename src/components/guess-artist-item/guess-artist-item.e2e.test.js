2ef339c59862e64d7a7ce54392ab96c4c38545d0
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-07 15:14:57 +0500

    Добавляет тесты в guess-artist-item

---
+++ src/components/guess-artist-item/guess-artist-item.e2e.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import {GuessArtistItem} from './guess-artist-item';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const question = {
+  type: `artist`,
+  song: {
+    artist: `One`,
+    src: ``
+  },
+  answers: [
+    {
+      picture: ``,
+      artist: `One`
+    },
+    {
+      picture: ``,
+      artist: `Two`
+    }
+  ]
+};
+
+describe(`GuessArtistItem e2e`, () => {
+  let clickHandler;
+  let screen;
+  let link;
+
+  beforeEach(() => {
+    const {answers: [{picture, artist}]} = question;
+
+    clickHandler = jest.fn();
+    screen = shallow(
+        <GuessArtistItem
+          picture={picture}
+          artist={artist}
+          onClick={clickHandler}
+          orderId={42}
+        />
+    );
+    link = screen.find(`.artist__input`);
+  });
+
+  it(`should have a link`, () => {
+    expect(link).toHaveLength(1);
+  });
+
+  describe(`before link is clicked`, () => {
+    it(`should not call clickHandler`, () => {
+      expect(clickHandler).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe(`after link is clicked`, () => {
+    beforeEach(() => {
+      link.simulate(`click`);
+    });
+
+    it(`should call clickHandler`, () => {
+      expect(clickHandler).toHaveBeenCalledTimes(1);
+    });
+  });
+});
