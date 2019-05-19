67b2a1771193f0a5b9d879be9b3012d6cd0a80bf
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-19 17:14:31 +0500

    Исправляет тесты для GameHeader

--- src/components/game-header/game-header.test.js
+++ src/components/game-header/game-header.test.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {GameHeader} from './game-header';
 import {create} from 'react-test-renderer';
 
+import {MockProvider} from '../../mocks/store';
+
 describe(`GameHeader`, () => {
   it(`should render correctly`, () => {
     const tree = create(
+      <MockProvider>
         <GameHeader
           onBackClick={() => null}
         />
+      </MockProvider>
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
